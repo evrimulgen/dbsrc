045aa686bb8f6e58edad89e5b420214e5a5f40e1
SteveRutledge <44811980+SteveRutledge@users.noreply.github.com>
2018-11-06 11:05:45 -0500

    Add files via upload

---
+++ bin/reconfig.bat
@@ -0,0 +1,45 @@
+
+::    Copyright 2019 Steve Rutledge
+::    =========================================================================
+::    This file is part of DBSRC Librarian.
+::
+::    DBSRC Librarian is free software: you can redistribute it and/or modify
+::    it under the terms of the GNU General Public License as published by
+::    the Free Software Foundation, version 3 of the License.
+::
+::    DBSRC Librarian is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU General Public License for more details.
+::
+::    You should have received a copy of the GNU General Public License
+::    along with DBSRC Librarian (see file COPYING.txt).  
+::    If not, see <https://www.gnu.org/licenses/>.
+::    =========================================================================
+
+@echo off
+setlocal enableDelayedExpansion
+
+:: test that prereqs are met
+if [%dbsrc_home%] == [] (
+	echo dbsrc_home=%dbsrc_home%
+	echo Not initialized. Please configure and execute init.bat in the dbsrc_home directory.
+	goto error
+)
+pushd .
+cd %dbsrc_home%
+
+:: execute all sql scripts in the config directory
+for /F %%x in ('dir /B %dbsrc_home%\cfg\*.sql') do (
+	set filename=%config_dir%\%%x
+	call sqlplus -s %dbsrc_user%/%dbsrc_pw%@%dbsrc_db% @cfg/!filename!
+	if %errorlevel% neq 0 goto error
+)
+goto end
+
+:error
+echo Aborting with Error!
+
+:end
+popd
+
